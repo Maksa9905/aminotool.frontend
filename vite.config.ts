301299d7c9f13c7acd6e713257d49bc84a1dc1e7
hakolr <m.gayvoronskiy@flang.io>
2025-06-11 14:47:49 +0300

    Configure for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,17 +1,18 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path, { dirname } from "path";
-import { fileURLToPath } from "url";
+import path, { dirname } from 'path'
+import { fileURLToPath } from 'url'
 
-const __filePath = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filePath);
+const __filePath = fileURLToPath(import.meta.url)
+const __dirname = dirname(__filePath)
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/aminotool.frontend/',
   resolve: {
     alias: {
-      "#": path.resolve(__dirname, "./src"),
+      '#': path.resolve(__dirname, './src'),
     },
   },
 })
